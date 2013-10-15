218e11e7edf3f3f5919f80eea20b45d49f9a7e95
seanpquig <seanpquig@gmail.com>
2013-10-15 17:14:12 -0400

    renamed lemon_script.py to lemon_DecTree.py

--- lemons_classifier/lemon_DecTree.py
+++ lemons_classifier/lemon_DecTree.py
@@ -4,6 +4,7 @@ a Car is a good buy (0) or bad buy (1)
 """
 
 ### Import Modules
+import numpy as np
 import pandas as pd
 from sklearn import metrics, tree, cross_validation
 
@@ -27,27 +28,30 @@ train_features = l_train[features].values
 train_class = l_train.IsBadBuy.values
 OSS_features = l_test[features].values
 
+# Seed PRNG
+np.random.seed(1234)
 X_train, X_test, y_train, y_test = \
     cross_validation.train_test_split(train_features, train_class, test_size=.3)
 
 
 ### Build model
-clf = tree.DecisionTreeClassifier().fit(X_train, y_train)
+clf = tree.DecisionTreeClassifier(max_depth=25, min_samples_leaf=2).fit(X_train, y_train)
 clf.score(X_train, y_train)
 y_pred_train = clf.predict(X_train)
 y_pred_test = clf.predict(X_test)
 
 
-print 'mislabeled points ', (train_y!=y_pred_train).sum()
-print metrics.confusion_matrix(y_train, y_pred_train)
-print metrics.confusion_matrix(y_test, y_pred_test)
+### Stats
+print 'training:\n', metrics.confusion_matrix(y_train, y_pred_train)
 print metrics.classification_report(y_train, y_pred_train)
+print 'test:\n', metrics.confusion_matrix(y_test, y_pred_test)
 print metrics.classification_report(y_test, y_pred_test)
-
+fpr_train, tpr_train, thresholds_train = metrics.roc_curve(y_train, y_pred_train, pos_label=1)
+fpr_test, tpr_test, thresholds_test = metrics.roc_curve(y_test, y_pred_test, pos_label=1)
+print 'test AUC: ', metrics.auc(fpr_train, tpr_train)
+print 'train AUC: ', metrics.auc(fpr_test, tpr_test)
 
 # y_pred = clf.predict(test_X)
 # submission = pd.DataFrame({ 'RefId' : l_test.RefId, 'prediction' : y_pred })
 # submission.to_csv('submission.csv')
 
-
-### 
